fee3ca453ff6108c4f07b635bd9d85d4e82cb1ee
Cyril Anne Argame <143865538+Argame22@users.noreply.github.com>
2024-08-31 17:21:47 +0800

    Add files via upload

---
+++ grp2_argame_ex1.py
@@ -0,0 +1,35 @@
+#Python Comments
+# a. Create a simple print script
+# b. Add comment "My first simple script"
+print("Hello, I'm Cyril Anne Argame! from BSCS 3G") #My first simple script
+
+#Python Variables
+# a. Create two variables and assign a number and a name to them
+number = 21
+name = "Cyril Anne"
+
+# b. Then print both values
+print("Name:",  name)
+print("Number:", number)
+
+#Python - Variable Names
+# a. Create 4 variables names using the first names of your group members. Your name should not be included.
+Groupmates = "Groupmates"
+Juliana = "Juliana"
+Kitt = "Kitt"
+Neo = "Neo"
+
+print("Groupmates:")
+print("Juliana")
+print("Kitt")
+print("Neo")
+
+# Many values to multiple variables
+# a. Assign values to multiple variables in one line
+x, y, z = 100, 200, 300
+print("x:",x, "y:",y ,"z:",z)
+
+# One value tp multiple Variables 
+# a. Assign the same value to multiple variables in one line
+a = b = c = "100"
+print("a:", a, "b:", b, "c:", c)
